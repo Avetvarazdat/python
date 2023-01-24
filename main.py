a641c84baab92ed164e429414d5bdfcb61dcc8c1
Avetvarazdat <avetvarazdat@gmail.com>
2023-01-24 19:22:40 +0400

    Our firts commit

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print("This is a main python file")
+print("This is  first change in local computer")
+print("This is  first change in local computer")
+print("This is  first change in local computer")
+print("This is  first change in local computer")
